7a09b306f4a9e4edaac60911047b220ae7bb7005
tusharmittal1 <35771172+tusharmittal1@users.noreply.github.com>
2018-04-04 17:09:32 +0100

    ntng

--- PitofSnake/app/src/main/java/kristof/pitofsnake/HomeScreen.java
+++ PitofSnake/app/src/main/java/kristof/pitofsnake/HomeScreen.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 public class HomeScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home_screen);
     }
     public void onClickSend(View view) {
