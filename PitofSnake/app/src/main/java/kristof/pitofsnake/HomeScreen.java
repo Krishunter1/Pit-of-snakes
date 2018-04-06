3013ec2344bdd3ef85252d8808bfde0b222db351
tusharmittal1 <35771172+tusharmittal1@users.noreply.github.com>
2018-04-06 17:03:12 +0100

    button and background sound

--- PitofSnake/app/src/main/java/kristof/pitofsnake/HomeScreen.java
+++ PitofSnake/app/src/main/java/kristof/pitofsnake/HomeScreen.java
@@ -2,6 +2,7 @@ package kristof.pitofsnake;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -14,13 +15,23 @@ public class HomeScreen extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home_screen);
+        MediaPlayer player = MediaPlayer.create(this, R.raw.backgroundsong);
+        player.setLooping(true);
+        player.start();
+
+
     }
+
     public void onClickSend(View view) {
+        MediaPlayer buttonsound = MediaPlayer.create(this, R.raw.buttonsound);
+        buttonsound.start();
         Intent intent = new Intent(this, MenuScreen.class);
         startActivity(intent);
     }
 
     public void onClickStart(View view) {
+        MediaPlayer buttonsound = MediaPlayer.create(this, R.raw.buttonsound);
+        buttonsound.start();
         Intent intent = new Intent(this, PitofSnake.class);
         startActivity(intent);
     }
--- PitofSnake/app/src/main/java/kristof/pitofsnake/MenuScreen.java
+++ PitofSnake/app/src/main/java/kristof/pitofsnake/MenuScreen.java
@@ -2,12 +2,15 @@ package kristof.pitofsnake;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MenuScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_menu_screen);
     }
 }
